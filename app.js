399f1c866aa978d16b190523e2cbf72ba39c017d
Emily81926 <emily81926@gmail.com>
2021-08-29 10:17:49 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -2,21 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true})
+require('./config/mongoose')
+
 const URL = require('./models/url')
 const generateRandomCode = require('./tools/generateRandomCode')
 const routes = require('./routes')
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 
 app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: 'hbs'}))
